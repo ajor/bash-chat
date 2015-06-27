48484d9fde5ae6cd48e57dd864192a8bd47ddef4
Alastair Robertson <alastair@ajor.co.uk>
2015-06-27 16:04:16 +0100

    Make sure participants see the same chat history

--- server.sh
+++ server.sh
@@ -21,6 +21,10 @@ clear_line() {
   printf '\r\033[2K'
 }
 
+move_cursor_up() {
+  printf '\033[1A'
+}
+
 server() {
   tail -f $output | nc -l -p $port > $input
   echo server ending
@@ -32,7 +36,9 @@ receive() {
   while IFS= read -r message; do
     clear_line
     printf '%s: %s\n%s: ' "$client_name" "$message" "$host_name"
-    printf '%s: ' "$client_name" > $output
+    move_cursor_up > $output
+    clear_line > $output
+    printf '%s: %s\n%s: ' "$client_name" "$message" "$client_name" > $output
   done < $input
   echo receive ending
 }
@@ -44,7 +50,9 @@ chat() {
     IFS= read -r message
     clear_line > $output
     printf '%s: %s\n%s: ' "$host_name" "$message" "$client_name" > $output
-    printf '%s: ' "$host_name"
+    move_cursor_up
+    clear_line
+    printf '%s: %s\n%s: ' "$host_name" "$message" "$host_name"
   done;
   echo chat ending
 }
